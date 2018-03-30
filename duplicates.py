9ab3a2f3201f23d07299148b8ee5c7db0e88c860
yurlov43 <yurlov43@yandex.ru>
2018-03-30 21:47:33 +0300

    Variables are called more suitable names

--- duplicates.py
+++ duplicates.py
@@ -17,29 +17,29 @@ def get_all_files(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
             file_size = os.path.getsize(file_path)
-            name = ('{}_{}'.format(file, file_size))
-            file_paths = all_files.get(name)
+            combining_name_size = ('{}..{}'.format(file, file_size))
+            file_paths = all_files.get(combining_name_size)
             if not file_paths:
                 file_paths = []
             file_paths.append(file_path)
-            all_files.update({name: file_paths.copy()})
+            all_files.update({combining_name_size: file_paths.copy()})
             file_paths.clear()
     return all_files
 
 
 def search_duplicates(all_files):
     duplicates = {}
-    for name, file_paths in all_files.items():
+    for combining_name_size, file_paths in all_files.items():
         if (len(file_paths) > 1):
-            duplicates.update({name: file_paths.copy()})
+            duplicates.update({combining_name_size: file_paths.copy()})
     return duplicates
 
 
 def print_the_result(duplicates, search_time):
     print('\nДубликаты:')
-    for duplicate_name, file_paths in duplicates.items():
-        file_name = duplicate_name.split('_')[0]
-        file_size = duplicate_name.split('_')[1]
+    for combining_name_size, file_paths in duplicates.items():
+        file_name = combining_name_size.split('..')[0]
+        file_size = combining_name_size.split('..')[1]
         print('\nФайл: {}'.format(file_name))
         print('Размер: {}'.format(file_size))
         for number, file_path in enumerate(file_paths, start=1):
